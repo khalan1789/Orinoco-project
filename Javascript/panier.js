0fb7b63716d8cf086cc692d713592e54addcc002
Ducau Benjamin <ducaubenjamin@gmail.com>
2021-06-02 09:56:33 +0200

    correction mise en page des fichiers js

--- Javascript/panier.js
+++ Javascript/panier.js
@@ -26,6 +26,7 @@ function basketNotEmpty(){
 let totalToPay = 0;
 const DisplayTotal = document.querySelector("#totalBasketPrice");
 const btnFormValid = document.querySelector("#btn-form-valid");
+
  // récupération et attribution des données dans le panier
 if(!basketOrder){
   basketEmpty();
@@ -36,24 +37,23 @@ if(!basketOrder){
     document.querySelector("#basketContain").insertAdjacentHTML("beforeend", `
       <div class="row mt-2 mb-3 border-bottom border-primary" data-id=${item.id}>
       <img src="${item.image}" class="col-12 col-md-4 col-lg-3" alt="photo de l'article">
-          <div class="d-flex flex-column col-md-5 mb-4 ">
-              <h4 class="col-sm mb-3 mt-3 text-center ">${item.name}</h4>
-              <label class="col-12 col-sm mt-3 text-center"> Quantité</label>
-              <div class="col-12 col-sm mt-4 d-flex justify-content-center">   
-                  <button type="button" id="btn-less" class="btn mr-4" aria-labelledby ="button less"><i class="fas fa-minus"></i></button>
-                  <input type="number" class="quantity" class="col-3 col-sm-4" max="100" value ="${item.quantity}" pattern="^[0-9]$"></input>
-                  <button type="button" id="btn-plus" class="btn ml-4" aria-labelledby ="button plus"><i class="fas fa-plus-square"></i></button>
-              </div>
-              </div>
-              <div class="d-flex justify-content-center flex-column col-md-2 text-center mb-3 mt-3">
-                <h4 class="col-sm">Prix Total</h4>
-                <p class="col-sm mt-2 total-item-price">${(item.price * item.quantity) / 100} \u20AC</p>
-              </div>
-              <button type="reset" class="btn reset-btn sm-ml-4 mb-3 col col-md-1"><i class="far fa-trash-alt"></i></button>
-          </div>
-          
+        <div class="d-flex flex-column col-md-5 mb-4 ">
+            <h4 class="col-sm mb-3 mt-3 text-center ">${item.name}</h4>
+            <label class="col-12 col-sm mt-3 text-center"> Quantité</label>
+            <div class="col-12 col-sm mt-4 d-flex justify-content-center">   
+                <button type="button" id="btn-less" class="btn mr-4" aria-labelledby ="button less"><i class="fas fa-minus"></i></button>
+                <input type="number" class="quantity" class="col-3 col-sm-4" max="100" value ="${item.quantity}" pattern="^[0-9]$"></input>
+                <button type="button" id="btn-plus" class="btn ml-4" aria-labelledby ="button plus"><i class="fas fa-plus-square"></i></button>
+            </div>
+            </div>
+            <div class="d-flex justify-content-center flex-column col-md-2 text-center mb-3 mt-3">
+              <h4 class="col-sm">Prix Total</h4>
+              <p class="col-sm mt-2 total-item-price">${(item.price * item.quantity) / 100} \u20AC</p>
+            </div>
+            <button type="reset" class="btn reset-btn sm-ml-4 mb-3 col col-md-1"><i class="far fa-trash-alt"></i></button>
+        </div>  
       `
-    )
+    );
     totalToPay += (item.price * item.quantity) / 100;
     // DisplayTotal.innerHTML = totalToPay + " €";
     totalCount();
@@ -68,71 +68,75 @@ const QuantityInput = document.querySelectorAll(".quantity");        //revoir pl
 const Plus = document.querySelectorAll("#btn-plus"); 
 const Less = document.querySelectorAll("#btn-less");
 const ItemTotalPrice = document.querySelectorAll(".total-item-price");
+
+function sendBasketOrderToStorage(){
+  localStorage.setItem("basketItems", JSON.stringify(basketOrder));
+}
+
 if(basketOrder){  
-    for(let i = 0; i < basketOrder.length; i ++){
-      const Item = basketOrder[i];
-      const LessBtn = Less[i];
-      const PlusBtn = Plus[i];
-      let quantity = QuantityInput[i];
-      const priceItem = basketOrder[i].price;
-      let NewItemTotalPrice = ItemTotalPrice[i];
+  for(let i = 0; i < basketOrder.length; i ++){
+    const Item = basketOrder[i];
+    const LessBtn = Less[i];
+    const PlusBtn = Plus[i];
+    let quantity = QuantityInput[i];
+    const priceItem = basketOrder[i].price;
+    let NewItemTotalPrice = ItemTotalPrice[i];
 
       //on supprime un de la quantité de l'article du panier
-      function lessQuantity(){
-        if (quantity.value > 1){
-          Item.quantity --;
-          console.log( Item.quantity);
-          //on met à jour le contenu du panier au localStorage en enlevant la quantité à l'article concerné
-          localStorage.setItem("basketItems", JSON.stringify(basketOrder));
-          //on affiche le changement de quantité à l'écran et on met le prix total de l'article à jour
-          quantity.value = Item.quantity;
-          NewItemTotalPrice.innerHTML = (parseInt(quantity.value, "10") * priceItem) / 100 + " \u20AC";
-          //on met à jour le prix total à payer
-          totalCount();
-        }
-      }
-      LessBtn.addEventListener("click", lessQuantity);
-
-      //on ajoute un à la quantité de l'article du panier
-      function plusQuantity(){
-        Item.quantity ++;
+    function lessQuantity(){
+      if (quantity.value > 1){
+        Item.quantity --;
         console.log( Item.quantity);
         //on met à jour le contenu du panier au localStorage en enlevant la quantité à l'article concerné
-        localStorage.setItem("basketItems", JSON.stringify(basketOrder));
+        sendBasketOrderToStorage();
         //on affiche le changement de quantité à l'écran et on met le prix total de l'article à jour
         quantity.value = Item.quantity;
         NewItemTotalPrice.innerHTML = (parseInt(quantity.value, "10") * priceItem) / 100 + " \u20AC";
-        //on met à jour le prix total à payer
+         //on met à jour le prix total à payer
         totalCount();
-
       }
-      PlusBtn.addEventListener("click", plusQuantity);
+    }
+    LessBtn.addEventListener("click", lessQuantity);
 
-      //Gestion des quantités lors de saisie par l'utlisateur               /********HERERERERERERRE */
-      quantity.addEventListener("change", function(){
-        // let quantityInput = this.value;
-        let quantityRegExp = new RegExp("^[0-9]{1,3}$");
-        let quantityTest = quantityRegExp.test(this.value)
-        if(quantityTest){
+    //on ajoute un à la quantité de l'article du panier
+    function plusQuantity(){
+      Item.quantity ++;
+      console.log( Item.quantity);
+      //on met à jour le contenu du panier au localStorage en enlevant la quantité à l'article concerné
+      sendBasketOrderToStorage();
+      //on affiche le changement de quantité à l'écran et on met le prix total de l'article à jour
+      quantity.value = Item.quantity;
+      NewItemTotalPrice.innerHTML = (parseInt(quantity.value, "10") * priceItem) / 100 + " \u20AC";
+      //on met à jour le prix total à payer
+      totalCount();
+    }
+    PlusBtn.addEventListener("click", plusQuantity);
+
+    //Gestion des quantités lors de saisie par l'utlisateur               /********HERERERERERERRE */
+    quantity.addEventListener("change", function(){
+      // let quantityInput = this.value;
+      let quantityRegExp = new RegExp("^[0-9]{1,3}$");
+      let quantityTest = quantityRegExp.test(this.value)
+      if(quantityTest){
         // la quantité saisie devient la valeur de l'article
         Item.quantity = parseInt(quantity.value, "10");
         //on met à jour le prix dans le panier dans le display
         NewItemTotalPrice.innerHTML = (parseInt(quantity.value, "10") * priceItem) / 100 + " \u20AC";
         // on met à jour les quantités de l'article
-        localStorage.setItem("basketItems", JSON.stringify(basketOrder));
+        sendBasketOrderToStorage();
         totalCount();
-        }
-        else{
+      }
+      else{
         alert("Saisie incorrecte pour cet article"); // A VOIR ICI
         this.value = 1;
         Item.quantity = parseInt(quantity.value, "10");
         NewItemTotalPrice.innerHTML = (parseInt(quantity.value, "10") * priceItem) / 100 + " \u20AC";
-        localStorage.setItem("basketItems", JSON.stringify(basketOrder));
+        sendBasketOrderToStorage();
         totalCount()
         }
     ;
     })
-    } 
+  } 
 }
 //calcul du prix total
 function totalCount() {
@@ -140,16 +144,16 @@ function totalCount() {
   for (let item of basketOrder)
   totalToPay += (item.price * item.quantity) / 100;
   DisplayTotal.innerHTML = totalToPay + " \u20AC";
-
 }
 
 /****RESET DE L'ARTICLE AU BOUTON ****/
+
 let resetBtnList = document.getElementsByClassName("reset-btn");
 
 //on récupère tous les boutons reset de la fenêtre
 if(basketOrder){
-for (let resetBtn of resetBtnList )
-resetBtn.addEventListener("click", ()=> {
+  for (let resetBtn of resetBtnList ){
+    resetBtn.addEventListener("click", ()=> {
       // on récupère l'id de l'élément à supprimer
     let parentId = resetBtn.parentElement.getAttribute("data-id");
      
@@ -173,7 +177,7 @@ resetBtn.addEventListener("click", ()=> {
 
     //on met à jour le local storage
     if (basketOrder.length >= 1) {
-      localStorage.setItem("basketItems", JSON.stringify(basketOrder));
+      sendBasketOrderToStorage();
     } else {
       localStorage.removeItem("basketItems");
       basketEmpty();
@@ -181,117 +185,113 @@ resetBtn.addEventListener("click", ()=> {
     
     //on met à jour le prix total
     totalCount();
-
-});
-
-}
+    });
+  };  
+};
 
 
 ///////////**********GESTION DU FORMULAIRE ***********///////////
+
 //fonction pour indiquer visuellement que ce n'est pas valide
 function ifDanger(value){
-  if (value.classList.contains("border-success")) {
+  if (value.classList.contains("border-success")){
     value.classList.remove("border-success");
-   }
-   value.classList.add("border-danger");
-}
+  }
+  value.classList.add("border-danger");
+};
+
 //fonction pour indiquer visuellement que c'est ok pour la saisie
 function ifSuccess(value){
   if(value.classList.contains("border-danger")){
     value.classList.remove("border-danger");
- }
- value.classList.add("border-success")
-}
+  }
+  value.classList.add("border-success")
+};
+
 //fonction pour enlever les indicateurs visuels si l'input redevient à 0 après une saisie
 function noMoreInput(element){
-  if (element.classList.contains("border-danger", "text-danger")) {
+  if (element.classList.contains("border-danger", "text-danger")){
     element.classList.remove("border-danger", "text-danger");
   }
-  else if (element.classList.contains("border-success", "text-success")){
+  else if(element.classList.contains("border-success", "text-success")){
     element.classList.remove("border-success", "text-success");
   }
-}
+};
 
 //vérification que tout est bien bon après la saisie dans un input
 let formInputs = document.querySelectorAll("#form-order input");
 for (let input of formInputs){
   input.addEventListener("change", function(){
-      //si la saisie est correcte ou non changer le visuel de l'imput
+    //si la saisie est correcte ou non changer le visuel de l'imput
     if(input.reportValidity()){
       ifSuccess(input);
-     
     }else{
       ifDanger(input);
     };
-  }
-  )
+  });
 };
 
 //actions de vérification du formulaire au moment de l'envoi    
 const FormOrder = document.querySelector("#form-order");                   /////ICI ENVOI *****
 FormOrder.addEventListener("submit", function(e){
   e.preventDefault();
-  //variable pour vérifier si la saisie est correcte et retourne true
+    //variable pour vérifier si la saisie est correcte et retourne true
   let inputValid = true;
-  //parcours de chacun des inputs pour vérification
+    //parcours de chacun des inputs pour vérification
   for (let input of document.querySelectorAll("#form-order input")){
-      
+    // vérification que les champs inputs saisis soient corrects   
     inputValid = inputValid && input.reportValidity;
     if(input.reportValidity()){
       ifSuccess(input);
-    inputValid = true;
-    console.log("l'input est " + inputValid);
+      inputValid = true;
+      console.log("l'input est " + inputValid);
     }else{
       ifDanger(input);
       inputValid = false;
       console.log("l'input doit être " + inputValid);
     };
-  } 
-  //si donc tous les imputs sont valides
+  }; 
+    //si donc tous les imputs sont valides
   if(inputValid){
-     
-      //récupération des infos et création de l'objet à envoyer
-      let contact = getUserInfo();
-      let dataOrder = {contact, products};
-             
-      console.log("newContact")
-      // console.log(contact);
       
-      console.log("products");
-      console.log(products);
-      console.log("objet a fetch")
-      console.log(dataOrder);
-      fetch('http://localhost:3000/api/teddies/order', {
-        method: "POST",
-        headers: { 
+    //récupération des infos et création de l'objet à envoyer
+    let contact = getUserInfo();
+    let dataOrder = {contact, products};
+    console.log("newContact")
+        // console.log(contact);
+        
+        console.log("products");
+        console.log(products);
+        console.log("objet a fetch")
+        console.log(dataOrder);
+    fetch('http://localhost:3000/api/teddies/order', {
+      method: "POST",
+      headers: { 
         'Accept': 'application/json', 
         'Content-Type': 'application/json', 
-       },
-       body: JSON.stringify(dataOrder)
-      })
-      .then(function(response){
-        if(response.ok){
-          return response.json()
-        }
-      })
-      .then(order => {
-        console.log("order");
-        console.log(order);
-        localStorage.setItem("order", JSON.stringify(order));
-        localStorage.removeItem("basketItems")
-        window.location.assign("order.html");
-      })
-      // .catch( (error) => {
-      //   console.log(error)});
-        
+      },
+      body: JSON.stringify(dataOrder)
+    })
+    .then(function(response){
+      if(response.ok){
+        return response.json()
+      }
+    })
+    .then(order => {
+      console.log("order");
+      console.log(order);
+      // on envoie la commande dans le localStorage, on supprime le panier et on dirige vers la page commande
+      localStorage.setItem("order", JSON.stringify(order));
+      localStorage.removeItem("basketItems")
+      window.location.assign("order.html");
+    })
   }
   e.stopPropagation();
-})
+});
 
 /****** MISE EN PLACE DE L'OBJET CONTACT ET DU TABLEAU POUR POST ******/
 
 //Objet contact
-
 class contact {
   constructor (lastName, firstName, address, city, email, postal, totalCount){
     this.lastName = lastName,
@@ -302,18 +302,18 @@ class contact {
     this.postal = postal,
     this.totalCount = totalCount
   }
-}
+};
 
 //Récupération des infops user pour le POST
- function getUserInfo(){
- let lastName = document.querySelector("#userName").value;
- let firstName =  document.querySelector("#userFirstName").value;
- let address =  document.querySelector("#userAddress").value;
- let city =  document.querySelector("#city").value;
- let email =  document.querySelector("#userEmail").value;
- let postal = document.querySelector("#postalCode").value;
- let totalCount = document.querySelector("#totalBasketPrice").innerHTML;
- 
- const user = new contact(lastName, firstName, address, city, email, postal, totalCount);
- return user
-}
+function getUserInfo(){
+  let lastName = document.querySelector("#userName").value;
+  let firstName =  document.querySelector("#userFirstName").value;
+  let address =  document.querySelector("#userAddress").value;
+  let city =  document.querySelector("#city").value;
+  let email =  document.querySelector("#userEmail").value;
+  let postal = document.querySelector("#postalCode").value;
+  let totalCount = document.querySelector("#totalBasketPrice").innerHTML;
+  
+  const user = new contact(lastName, firstName, address, city, email, postal, totalCount);
+  return user
+};
--- Javascript/product.js
+++ Javascript/product.js
@@ -12,7 +12,9 @@ const BtnSendToBasket = document.querySelector("#btn-sendToBasket"); //bouton é
 
 fetch(ApiToFetch + "/" + Id)
   .then(response => {
-    if (response.ok){return response.json()}
+    if (response.ok){
+      return response.json()
+    }
     else{
       console.log("erreur :" + response.status)
     }
@@ -22,96 +24,93 @@ fetch(ApiToFetch + "/" + Id)
     window.location.assign("error.html");
   })
   .then(teddyItem => {
-        console.log (teddyItem)
+    console.log (teddyItem)
         
-        document.querySelector(".product-title").innerText = teddyItem.name;
-        document.querySelector(".product-description").innerText = teddyItem.description;
-        document.querySelector(".product-price").innerText = "Prix : " + teddyItem.price / 100 + " \u20AC";
-        document.querySelector(".product-img").innerHTML = `<img src="${teddyItem.imageUrl}" alt="">`;
-        document.querySelector(".product-specification").setAttribute("id", teddyItem._id);
-        document.querySelector(".quantity").value = parseInt(quantityInput.value, "10");
-        //recuperation des couleurs en forme de liste déroulante
-        for (let teddyColor of teddyItem.colors){
-        document.querySelector("#color-list").innerHTML += 
-        "<option>" + teddyColor + "</option>";   
-        }             
+    document.querySelector(".product-title").innerText = teddyItem.name;
+    document.querySelector(".product-description").innerText = teddyItem.description;
+    document.querySelector(".product-price").innerText = "Prix : " + teddyItem.price / 100 + " \u20AC";
+    document.querySelector(".product-img").innerHTML = `<img src="${teddyItem.imageUrl}" alt="">`;
+    document.querySelector(".product-specification").setAttribute("id", teddyItem._id);
+    document.querySelector(".quantity").value = parseInt(quantityInput.value, "10");
+      //recuperation des couleurs en forme de liste déroulante
+    for (let teddyColor of teddyItem.colors){
+      document.querySelector("#color-list").innerHTML += 
+      "<option>" + teddyColor + "</option>";   
+    }             
         
    /************ Ajout d'un produit dans le localStorage*********/
 
     BtnSendToBasket.addEventListener("click", ()=>{ 
     //Création de l'objet à mettre dans le panier    
-        let dataTeddyItem = {
-            name : teddyItem.name,
-            description : teddyItem.description,
-            id : teddyItem._id,
-            price : teddyItem.price,
-            image : teddyItem.imageUrl,
-            quantity : parseInt(quantityInput.value, "10")
-        }
-        
-        console.log(dataTeddyItem)
+      let dataTeddyItem = {
+        name : teddyItem.name,
+        description : teddyItem.description,
+        id : teddyItem._id,
+        price : teddyItem.price,
+        image : teddyItem.imageUrl,
+        quantity : parseInt(quantityInput.value, "10")
+      }
+      console.log(dataTeddyItem)
         //Si le panier du Storage est vide    
-        if (!basketOrder){ 
-            //on crée le panier et on met l'objet avec la quantité et ses infos dedans
-            basketOrder = [];
-            basketOrder.push(dataTeddyItem)
+      if (!basketOrder){ 
+         //on crée le panier et on met l'objet avec la quantité et ses infos dedans
+        basketOrder = [];
+        basketOrder.push(dataTeddyItem)
             
-            //on l'envoi au local storage
-            localStorage.setItem("basketItems", JSON.stringify(basketOrder));
-        }
-        else{ //sinon s'il y a déjà le panier dans le local storage
+        //on l'envoi au local storage
+        localStorage.setItem("basketItems", JSON.stringify(basketOrder));
+      }
+      else{ //sinon s'il y a déjà le panier dans le local storage
         //   variable pour savoir s'il existe  
-            let isAlreadyExist = false;
+        let isAlreadyExist = false;
 
-            //vérification des objets du panier
-            for (let item of basketOrder){
+         //vérification des objets du panier
+          for (let item of basketOrder){
             // si oui il y est déjà
-                if(item.name == dataTeddyItem.name){
-                   isAlreadyExist = true;
-                   console.log(item.name);
-                    // on ajoute la quantité saisie à celle déjà existante pour l'objet
-                  item.quantity += parseInt(quantityInput.value, "10");
-                  console.log(item.quantity)
-                //   et on met à jour le storage
-                  localStorage.setItem("basketItems", JSON.stringify(basketOrder));
-                }
-            };                       
-
-            // s'il n'y est pas je rajoute l'obet et j'envoie au storage
-            if(!isAlreadyExist){
-              basketOrder.push(dataTeddyItem);
+            if(item.name == dataTeddyItem.name){
+              isAlreadyExist = true;
+              console.log(item.name);
+              // on ajoute la quantité saisie à celle déjà existante pour l'objet
+              item.quantity += parseInt(quantityInput.value, "10");
+              console.log(item.quantity)
+              //   et on met à jour le storage
               localStorage.setItem("basketItems", JSON.stringify(basketOrder));
-            }
-        }
-    })     
+            };
+          };                       
+
+            // si l'objet n'est pas dans le tableau, on l'ajoute et on envoie au storage
+          if(!isAlreadyExist){
+            basketOrder.push(dataTeddyItem);
+            localStorage.setItem("basketItems", JSON.stringify(basketOrder));
+          };
+      };
+    });     
   }); 
 
    /******* Fonctions pour ajouter supprimer un artcile et afficher la quantité dans l'input ***** */
-//  const BtnSendToBasket = document.querySelector("#btn-sendToBasket"); //bouton écouté
 
 let quantityInput = document.querySelector(".quantity");
-        //revoir plus tard pour mettre une regExp pour l'input
 let plus = document.querySelector("#btn-plus"); 
 let less = document.querySelector("#btn-less");
 
 // // ajout de 1 dans le nombre d'articles dans l'input
 function plusQuantity(){
- let quantity = parseInt(quantityInput.value, "10");
+  let quantity = parseInt(quantityInput.value, "10");
   if(quantity < 100){
   quantity += 1 ;
   }
   quantityInput.value = quantity;
-  console.log("type de qty :" + typeof(quantity));
 }
+
 // // suppression de 1 dans le nombre d'articles dans l'input
 function lessQuantity(){
   let quantity = parseInt(quantityInput.value, "10");
   if (quantityInput.value > 1){
     quantity -= 1;
-    console.log("type de qte - :" + typeof(quantity));
   }
   quantityInput.value = quantity;
 };
+
 //gestion du format des quantités dans l'input
 function formatQuantity(){
   let quantity = parseInt(quantityInput.value);
@@ -134,15 +133,16 @@ function formatQuantity(){
   quantityInput.value = quantity;
 }
 
-plus.addEventListener("click", (e) => {
+plus.addEventListener("click", () => {
   plusQuantity();
   formatQuantity();
 });
-less.addEventListener("click", (e) => {
+less.addEventListener("click", () => {
   lessQuantity();
   formatQuantity();
 });
 
+//appel de la fonction format pour qu'à l'affichage le bouton moins soit désactivé au visuel
 formatQuantity();
 
 quantityInput.addEventListener("change", () =>{
@@ -155,128 +155,4 @@ quantityInput.addEventListener("change", () =>{
   }
 });
 
-// plus.addEventListener("click", function(e){
-//   let quantity = parseInt(quantityInput.value, "10");
-//   if(quantity >= 100 || quantity < 1){
-//       e.preventDefault();
-//       alert("Veuillez ressaisir un nombre correct compris entre 1 et 100")
-//   }else{
-//   ableLess();
-//   plusQuantity();
-//   console.log(quantity)
-//   console.log("type de qte :" + typeof(quantity));
-//   }
-  
-// });
 
-
-// less.addEventListener("click", lessQuantity);
-
-// ecoute de la quantité saisie
-// quantityInput.addEventListener("change", function(){
-//   quantity = parseInt(quantityInput.value,"10");
-//   verifInput(quantityInput)
-//   this.reportValidity()
-//   let quantity = parseInt(quantityInput.value, "10");
-//   if(quantity === 1){
-//     disableLess();
-//   }else{
-//     ableLess();
-//     quantity = quantityInput.value;
-//   }
-//   // less.addEventListener("click", lessQuantity);
-//   // plus.addEventListener("click", plusQuantity);
-
-
-// changé là
-// quantityInput.addEventListener("input", function(){
-//   let quantity = parseInt(this.value, "10");           // PROBLEME DE VISION DES QTE -1 PAR LA MACHINE
-//   this.reportValidity();
-//   quantityInput.value = quantity;
-//   if (quantity <= 0 || quantity > 100){
-//       BtnSendToBasket.setAttribute("disabled", "");
-//   }
-//   else if (quantity <= 1) {
-//       disableLess();
-//   }
-//   else{
-//       ableLess();
-//       BtnSendToBasket.removeAttribute("disabled");
-//   };
-//   // return qte
-  
-//   console.log(quantity)
-//   console.log(typeof(quantity));
-// });
-
-
-//////////***** Gestion de l'input *****////////
-
-
-// function verifInput(quantityInput){
-//  //création de la regExp pour contrôler la saisie 
-//  let inputRegexp = new RegExp ("^[0-9]{1,3}$");
-
-// if(!inputRegexp.test(quantityInput.value)){
-//     quantityInput.classList.add("text-danger");
-//     BtnSendToBasket.setAttribute("disabled", "");
-//     console.log("non pas bon la qte")
-//  }
-//  else{
-//    if(inputRegexp.test(quantityInput.value)){
-//   quantityInput.classList.remove("text-danger")
-//   console.log("ok good")
-//   BtnSendToBasket.removeAttribute("disabled");
-//    }
-//  }
-// }
-
-  // })
-// });
-
-//////**************VERSION MEDHI  TEST ******************/
-// plus.addEventListener("click", (e) => {
-//   addQte();
-//   formatQte();
-// });
-// less.addEventListener("click", (e) => {
-//   subQte();
-//   formatQte();
-// });
-
-// function formatQte() {
-//   let qte = parseInt(quantityInput.value);
-//   if (qte < 1) {
-//     qte = 1;
-//   } else if (qte > 100) {
-//     qte = 100;
-//   }
-//   if (qte === 1) {
-//     less.setAttribute("disabled", true);
-//   } else if (qte === 100) {
-//     plus.setAttribute("disabled", true);
-//   }
-//   if (qte > 1) {
-//     less.removeAttribute("disabled");
-//   }
-//   if (qte < 100) {
-//     plus.removeAttribute("disabled");
-//   }
-//   quantityInput.value = qte;
-// }
-// function addQte() {
-//   let qte = parseInt(quantityInput.value);
-//   if (qte < 99) {
-//     qte+= 1;
-//   }
-//   quantityInput.value = qte;
-// }
-// function subQte() {
-//   let qte = parseInt(quantityInput.value);
-//   if (qte > 1) {
-//     qte-= 1;
-//   }
-//   quantityInput.value = qte;
-// }
-// formatQte();
-// quantityInput.addEventListener("input", formatQte);
